7384ed46bd73b34e43fe8246b517333cbec5da2c
Camily <camilycastellimarcelino@gmail.com>
2025-09-09 08:46:31 -0300

    09/09

---
+++ Perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em termos de área?',
+    'Rússia'
+)
+
+criaCartao(
+    'Química',
+    'perguntaQual é o elemento químico com o símbolo "S"?',
+    'Enxofre'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    ' Qual é o nome da rede social criada por Mark Zuckerberg?',
+    'Facebook'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    ' Quem pintou a famosa obra "A Noite Estrelada"?',
+    'Vincent van Gogh'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    ' Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    ' Qual é o nome da rede social criada por Mark Zuckerberg?',
+    'Facebook'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    ' Qual é o nome da rede social criada por Mark Zuckerberg?',
+    'Facebook'
+)
