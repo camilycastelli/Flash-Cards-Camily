8864cd8480fdff6d440d04a46c8b1116fa07ca89
Camily <camilycastellimarcelino@gmail.com>
2025-09-23 09:08:52 -0300

    23/09

--- Perguntas.js
+++ Perguntas.js
@@ -1,72 +1,62 @@
 criaCartao(
-    'Geografia',
+    'geografia',
     'Qual é o maior país do mundo em termos de área?',
     'Rússia'
 )
 
 criaCartao(
     'Química',
-    'perguntaQual é o elemento químico com o símbolo "S"?',
+    'Qual é o elemento químico com o símbolo "S"?',
     'Enxofre'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
-    ' Qual é o nome da rede social criada por Mark Zuckerberg?',
+    'Gerais',
+    'Qual é o nome da rede social criada por Mark Zuckerberg?',
     'Facebook'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
-    ' Quem pintou a famosa obra "A Noite Estrelada"?',
-    'Vincent van Gogh'
+    'Gerais',
+    'Quem pintou a famosa obra "A Noite Estrelada"?',
+    'Vincent van Gogh.'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
-    ' Onde se localiza Machu Picchu?',
-    'Peru'
-)
-
-criaCartao(
-    'Conhecimentos gerais',
-    ' Qual é o nome da rede social criada por Mark Zuckerberg?',
-    'Facebook'
-)
-
-
-criaCartao(
-    'Conhecimentos gerais',
-    'Quem pintou a famosa obra "Noite Estrelada',
-    'Vicente Van Gogh'
-)
-
-criaCartao(
-    'Conhecimentos gerais',
+    'Geografia',
     'Onde se localiza Machu Picchu?',
     'Peru'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
+    'Gerais',
     'Quem inventou a lâmpada?',
-    'Thomas Edison'
+    'Thomas Edison.'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
-    'A que temperatura a água ferve?',
-    '100 graus'
+    'Gerais',
+    ' A que temperatura a água ferve?',
+    '100 graus.'
 )
 
 criaCartao(
-    'Qual o maior planeta do sistema solar?'
+    'Gerais',
+    'Qual o maior planeta do sistema solar?',
+    'jupiter.'
 )
 
+criaCartao(
+    'Gerais',
+    'Qual o monumento famoso pela sua inclinação?',
+    'Torre de pisa.'
+)
 
-
-
-
+criaCartao(
+    'Gerais',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin.'
+)
 
 
 
