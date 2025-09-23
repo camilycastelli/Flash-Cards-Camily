5c80c8832abb84000544fc0ef36ba4727b4d82b1
Camily <camilycastellimarcelino@gmail.com>
2025-09-23 10:33:36 -0300

    23/09

--- app.js
+++ app.js
@@ -18,11 +18,14 @@ let respostaEstaVisivel= false
 
 function viraCartao(){
     respostaEstaVisivel=!respostaEstaVisivel
+    cartao.classList.toggle('active',respostaEstaVisivel)
 }
 
+cartao.addEventListener('click', viraCartao)
 
     container.appendChild(cartao)
 
 
 
 }
+
